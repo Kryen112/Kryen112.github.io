70cf082ab08999bb7174f71e71da241178b3f50f
Kryen112 <Kryen112@hotmail.com>
2025-05-11 18:53:20 +0200

    Prioritize selecting the selected ranger when they are stacked on top of each other

--- public/game.js
+++ public/game.js
@@ -5246,6 +5246,7 @@ window.fff = SR_Player.prototype.PLsetHeldChar;
 SR_Player.prototype.PLsetHeldChar = function(){ // Pg.prototype.jb
     var dist_vector = new Vector2D;
     var grab_reach = 20;
+    var grab_reach_selected = 8;
     var dist_from_cursor;
     var prev_dist_from_cursor = grab_reach;
 
@@ -5258,6 +5259,13 @@ SR_Player.prototype.PLsetHeldChar = function(){ // Pg.prototype.jb
                 dist_vector.x = Mouse_Xpos-this.PL_joint_destination[s][j].x;
                 dist_vector.y = Mouse_Ypos-this.PL_joint_destination[s][j].y;
                 dist_from_cursor = magnitudeOf(dist_vector);
+                // If it's the selected player and it's within tighter radius, always grab
+                if (s===Selected_Player && dist_from_cursor<grab_reach_selected && (LP_Current[s]!=0 || Sett_Drag_Dead_Body!=0)) {
+                    this.PL_held_player = s;
+                    this.PL_held_joint = j;
+                    return;
+                }
+                // Otherwise do regular "closest found" logic
                 if (dist_from_cursor<grab_reach && dist_from_cursor<prev_dist_from_cursor && (LP_Current[s]!=0 || Sett_Drag_Dead_Body!=0)){
                     prev_dist_from_cursor = dist_from_cursor;
                     this.PL_held_player = s;
